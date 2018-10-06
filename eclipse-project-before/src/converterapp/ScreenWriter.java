517417fea88c080391797450f653b231429cc650
santii810 <santii810@hotmail.com>
2018-10-06 19:41:11 +0200

    Modificaciones de documentacion en practica 1

---
+++ eclipse-project-before/src/converterapp/ScreenWriter.java
@@ -0,0 +1,15 @@
+package converterapp;
+
+public class ScreenWriter implements Writer {
+
+	@Override
+	public void write(String toWrite) {
+		System.out.println(toWrite);
+	}
+
+	@Override
+	public void closeWriteWork() {
+		// Not necesary
+	}
+
+}
